bc5766151e070c699f905175bb4640f6273bd686
Thimira Dilshan <thimirad.ml@gmail.com>
2026-05-01 19:01:46 +0530

    Fix API URL trailing slash

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const API = 'https://workhub-api-production.up.railway.app/'
+const API = 'https://workhub-api-production.up.railway.app'
 
 function Login() {
   const [form, setForm] = useState({ email: '', password: '' })
